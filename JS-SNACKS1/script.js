c7d859a380a1054c8a399558373fa4e16c549255
L-V-Dragon <viotti37@gmail.com>
2022-11-14 15:33:48 +0100

    Fine JS1

--- JS-SNACKS1/script.js
+++ JS-SNACKS1/script.js
@@ -1,5 +1,14 @@
 //L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.
 
-let num1 = prompt ("Inserisci un numero");
-let num2 = prompt ("Inserisci un altro numero");
+let num1 = prompt("Inserisci un numero");
+let num2 = prompt("Inserisci un altro numero");
 
+if(num1>num2){
+    console.log("Il numero più grande è: " + num1);
+} else if(num2>num1){
+    console.log("Il numero più grande è: " + num2);
+} else if(num1 == num2){
+    console.log("I numeri sono uguali");
+} else{
+    console.log("Errore");
+}
