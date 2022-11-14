d81c52ead55c7bc6013cc61f26f45ae18647bc9c
L-V-Dragon <viotti37@gmail.com>
2022-11-14 15:05:13 +0100

    Creazione delle cartelle

---
+++ JS-SNACKS1/script.js
@@ -0,0 +1,5 @@
+//L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.
+
+let num1 = prompt ("Inserisci un numero");
+let num2 = prompt ("Inserisci un altro numero");
+
