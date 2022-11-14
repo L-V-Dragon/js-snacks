6c8d0eef515f4b28ffec2c35fec52409dff36ec4
L-V-Dragon <viotti37@gmail.com>
2022-11-14 15:58:31 +0100

    Completato JS3

---
+++ JS-SNACKS3/script.js
@@ -0,0 +1,11 @@
+let n;
+let somma = 0;
+let media;
+
+for (i = 0; i < 10; i++) {
+    n = parseInt(prompt('Inserisci un numero'));
+    somma += n;
+}
+
+media = somma / i;
+console.log('la media è ', media);
